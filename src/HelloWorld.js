9790479cd5d607009cc30d0704d72f93e20b7e83
cawauchi6204 <cawauchi6204@gmail.com>
2020-06-30 22:13:04 +0900

    stateの変更のさせ方を理解

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -13,10 +13,14 @@ class CountApp extends React.Component {
     }
 
     minus() {
+        if (this.state.count < 1 ) return;
         this.setState({ count: this.state.count - 1 })
     }
 
-    // setState関数というものを呼び出して、stateを更新
+    // setState関数を呼び出して、stateを更新
+
+    // stateを直接変更してはいけない。
+    // this.state.count = this.state.count + 1  // これはアウト！！
 
     render() {
         return (
@@ -24,12 +28,15 @@ class CountApp extends React.Component {
                 <h1>カウンターApp</h1>
                 <p>{this.state.count}</p>
                 <div>
-                    <button>+</button>
-                    <button>-</button>
+                    <button onClick={() => this.plus()}>+</button>
+                    <button onClick={() => this.minus()}>-</button>
                 </div>
             </>
         )
     }
+
+    // render関数はpureな関数であるべきとされています。どういうことかというと、render関数内でstateを変更してはいけないということです。
+    // なぜかというと、stateの変更が起こるとrender関数が呼ばれるようになっているので無限ループになってしまいます。
 }
 
 export default CountApp;
